91c15fbfc0198680b9cc9e0b9d40a446d7b26553
Chad Button <innovativeObjects@gmail.com>
2019-04-16 09:42:42 -0700

    all done except download

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,66 @@
 <template>
   <v-app id="sandbox" :dark="dark">
     <v-toolbar :clipped-left="primaryDrawer.clipped" app absolute>
-      <v-toolbar-title>Image Builder</v-toolbar-title>
+        <v-toolbar-title>Image Builder</v-toolbar-title>
     </v-toolbar>
     <v-content>
       <v-container fluid>
+         <v-switch v-model="dark" primary label="Dark"></v-switch>
         <v-layout align-center justify-center>
           <v-flex xs10>
             <v-card>
               <v-card-text>
                 <v-layout row wrap>
-                  <v-flex xs12 md6>
-                    <v-switch v-model="dark" primary label="Dark"></v-switch>
+                  <v-flex xs12 md2 style="padding:10px">
                     <v-text-field
-                      v-model="firstname"
-                      :rules="nameRules"
-                      :counter="10"
-                      label="First name"
+                      v-model="stock.ticker"
+                      label="Ticker"
                       required
-                  ></v-text-field>
+                      ></v-text-field>
+                      <v-text-field
+                      v-model="stock.date"
+                      label="Date"
+                      required
+                      ></v-text-field>
+                      <v-text-field
+                      v-model="stock.time"
+                      label="Time"
+                      required
+                      ></v-text-field>
+                  </v-flex>
+                  <v-flex xs12 md2 style="padding:10px">
+                      <v-text-field
+                      v-model="stock.price"
+                      label="Idea Price"
+                      required
+                      ></v-text-field>
+                      <v-text-field
+                      v-model="stock.high"
+                      label="Price High"
+                      required
+                      ></v-text-field>
+                      <v-text-field
+                      v-model="stock.sector"
+                      label="Sector"
+                      ></v-text-field>
+                      </v-flex>
+                  <v-flex xs12 md2 style="padding:10px">
+                      <v-text-field
+                      v-model="stock.volume"
+                      label="Volume"
+                      required
+                      ></v-text-field>
+                      <v-text-field
+                      v-model="stock.calledby"
+                      label="Called By"
+                      required
+                      ></v-text-field>
+                  </v-flex>
+                  <v-flex xs12 md2 style="padding:10px">
+                     <upload-btn :uniqueId="'btnUploadChart'" :fileChangedCallback="chartFileChanged" :title="chartUploadButtonTitle"
+                      ></upload-btn>
+                     <upload-btn :uniqueId="'btnUploadIdea'" :fileChangedCallback="ideaFileChanged" :title="ideaUploadButtonTitle"
+                      ></upload-btn>
                   </v-flex>
                  
                 </v-layout>
@@ -26,7 +68,7 @@
               <v-card-actions>
                 <v-spacer></v-spacer>
                 <v-btn flat>Cancel</v-btn>
-                <v-btn flat color="primary">Submit</v-btn>
+                <v-btn flat color="primary">Save</v-btn>
               </v-card-actions>
             </v-card>
           </v-flex>
@@ -39,7 +81,7 @@
               <v-card-text>
                 <div class="content">
                   <img class="logo" src="./ilslogo.png">
-                  <img class="chart-img" src="./chart.png"></img>
+                  <img class="chart-img" :src="chartFile.url"/>
                   <div class="stock-box">
                           <h3>PLAY OF THE DAY</h3>
                           <h1>{{stock.ticker}}</h1>
@@ -65,7 +107,7 @@
                           <td class="stock-return">{{stock.maxreturn}}%</td>
                       </tr>
                   </table>
-                  <img class="calledby-img" src="./calledby.png"/>
+                  <img class="calledby-img" :src="ideaFile.url"/>
               </div>
               </v-card-text>
             </v-card>
@@ -81,9 +123,63 @@
 </template>
 
 <script>
+  import UploadButton from 'vuetify-upload-button'
+
   export default {
+    methods: {
+      chartFileChanged (file) {
+        
+        if(file !== undefined) {
+          this.chartFile.name = file.name
+          if(this.chartFile.name.lastIndexOf('.') <= 0) {
+            return
+          }
+
+          console.log(file)
+          const fr = new FileReader ()
+          fr.readAsDataURL(file)
+          fr.addEventListener('load', () => {
+            this.chartFile.url = fr.result
+            this.chartFile.file = file // this is an image file that can be sent to server...
+          })
+        } else {
+          console.log('file was undefined')
+          this.chartFile.name = ''
+          this.chartFile.file = ''
+          this.chartFile.url = ''
+        }
+      },
+      ideaFileChanged (file) {
+        
+        console.log(file)
+
+        if(file !== undefined) {
+          this.ideaFile.name = file.name
+          if(this.ideaFile.name.lastIndexOf('.') <= 0) {
+            return
+          }
+          const fr = new FileReader ()
+          fr.readAsDataURL(file)
+          fr.addEventListener('load', () => {
+            this.ideaFile.url = fr.result
+            this.ideaFile.file = file // this is an image file that can be sent to server...
+          })
+        } else {
+          console.log('file was undefined')
+          
+          this.ideaFile.name = ''
+          this.ideaFile.file = ''
+          this.ideaFile.url = ''
+        }
+      }
+    },
+    components: {
+      'upload-btn': UploadButton
+    },
     data: () => ({
       dark: true,
+      chartUploadButtonTitle: 'Add Chart File',
+      ideaUploadButtonTitle: 'Add Idea File',
       stock: {
         ticker: 'ZSAN',
         date: '02/21/19',
@@ -94,6 +190,16 @@
         volume: '39.9k',
         calledby: 'Vegas',
         maxreturn: 130
+      },
+      ideaFile: {
+        name: '',
+        file: '',
+        url: ''
+      },
+      chartFile: {
+        name: '',
+        file: '',
+        url: ''
       },
       drawers: ['Default (no property)', 'Permanent', 'Temporary'],
       primaryDrawer: {
--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import vuecustomElement from 'vue-custom-element'
+import UploadButton from 'vuetify-upload-button';
 
 require('./assets/css/style.css')
 
+Vue.use(vuecustomElement)
+Vue.customElement('upload-btn', UploadButton)
+
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
