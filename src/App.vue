8808bbe061cafd6d8cc24558e62d547da4a3f475
Chad Button <innovativeObjects@gmail.com>
2019-04-16 07:53:47 -0700

    getting there

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,6 @@
 <template>
   <v-app id="sandbox" :dark="dark">
-    <v-navigation-drawer
-      v-model="primaryDrawer.model"
-      :permanent="primaryDrawer.type === 'permanent'"
-      :temporary="primaryDrawer.type === 'temporary'"
-      :clipped="primaryDrawer.clipped"
-      :floating="primaryDrawer.floating"
-      :mini-variant="primaryDrawer.mini"
-      absolute
-      overflow
-      app
-    ></v-navigation-drawer>
     <v-toolbar :clipped-left="primaryDrawer.clipped" app absolute>
-      <v-toolbar-side-icon
-        v-if="primaryDrawer.type !== 'permanent'"
-        @click.stop="primaryDrawer.model = !primaryDrawer.model"
-      ></v-toolbar-side-icon>
       <v-toolbar-title>Image Builder</v-toolbar-title>
     </v-toolbar>
     <v-content>
@@ -28,11 +13,11 @@
                   <v-flex xs12 md6>
                     <v-switch v-model="dark" primary label="Dark"></v-switch>
                     <v-text-field
-                    v-model="firstname"
-                    :rules="nameRules"
-                    :counter="10"
-                    label="First name"
-                    required
+                      v-model="firstname"
+                      :rules="nameRules"
+                      :counter="10"
+                      label="First name"
+                      required
                   ></v-text-field>
                   </v-flex>
                  
@@ -47,9 +32,50 @@
           </v-flex>
         </v-layout>
       </v-container>
+      <v-container fluid>
+        <v-layout align-center justify-center>
+          <v-flex xs10>
+            <v-card>
+              <v-card-text>
+                <div class="content">
+                  <img class="logo" src="./ilslogo.png">
+                  <img class="chart-img" src="./chart.png"></img>
+                  <div class="stock-box">
+                          <h3>PLAY OF THE DAY</h3>
+                          <h1>{{stock.ticker}}</h1>
+                          <h3>{{stock.date}}</h3>
+                  </div>
+                  <table cellpadding=0 cellspacing=0 class="stock-stats">
+                      <tr>
+                          <th class="idea">IDEA:</th>
+                          <th class="high" >HIGH:</th> 
+                          <th>SECTOR:</th>
+                          <th>VOLUME:</th>
+                          <th>CALLED BY:</th>
+                          <th>TIME:</th>
+                          <th>MAX RETURN</th>
+                      </tr>
+                      <tr>
+                          <td class="idea-value">{{stock.price}}</td>
+                          <td class="high-value">{{stock.high}}</td> 
+                          <td>{{stock.sector}}</td>
+                          <td>{{stock.volume}}</td>
+                          <td>{{stock.calledby}}</td>
+                          <td>{{stock.time}}</td>
+                          <td class="stock-return">{{stock.maxreturn}}%</td>
+                      </tr>
+                  </table>
+                  <img class="calledby-img" src="./calledby.png"/>
+              </div>
+              </v-card-text>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+      
     </v-content>
     <v-footer :inset="footer.inset" app>
-      <span class="px-3">&copy; {{ 'iluvstocks' + new Date().getFullYear() }}</span>
+      <span class="px-3">&copy; {{ 'iluvstocks ' + new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
@@ -58,6 +84,17 @@
   export default {
     data: () => ({
       dark: true,
+      stock: {
+        ticker: 'ZSAN',
+        date: '02/21/19',
+        time: '9:30 am',
+        price: 2.89,
+        high: 6.69,
+        sector: 'Pharma',
+        volume: '39.9k',
+        calledby: 'Vegas',
+        maxreturn: 130
+      },
       drawers: ['Default (no property)', 'Permanent', 'Temporary'],
       primaryDrawer: {
         model: null,
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
 
+require('./assets/css/style.css')
+
 Vue.config.productionTip = false
 
 new Vue({
